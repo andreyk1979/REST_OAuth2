1451c2da187eaa5adff4d3b47180c593f6c788df
andreyk1979 <andrey.kuimov@gmail.com>
2022-05-27 13:03:58 +0200

    restore pakage exception

---
+++ src/main/java/com/kuimov/pp/task314rest/exception/UsersIncorrectData.java
@@ -0,0 +1,16 @@
+package com.kuimov.pp.task314rest.exception;
+
+public class UsersIncorrectData {
+    private String info;
+
+    public UsersIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
