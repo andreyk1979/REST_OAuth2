812cb3466b519d64ee4f05578f5ac7d1f974afe8
andreyk1979 <andrey.kuimov@gmail.com>
2022-06-08 18:48:56 +0200

    create CSS for exampels

---
+++ src/main/java/com/kuimov/pp/task314rest/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.kuimov.pp.task314rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String showIndex() {
+        return "/index";
+    }
+}
+
