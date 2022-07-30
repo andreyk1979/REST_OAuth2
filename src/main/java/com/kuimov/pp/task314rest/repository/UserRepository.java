9e2459ec71ac8e40967ceac4ddef61843ee02aec
andreyk1979 <andrey.kuimov@gmail.com>
2022-07-30 09:57:16 +0200

    Remove duplicated directory

--- src/main/java/com/kuimov/pp/task314rest/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kuimov.pp.task314rest.repository;
-
-import com.kuimov.pp.task314rest.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User getUserByEmail(String email);
-}
